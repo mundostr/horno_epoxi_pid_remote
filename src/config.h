75952500f402a5c930a8b28dac5d5e33dc9f0767
Carlos Perren <idux.net@gmail.com>
2024-11-17 13:47:43 -0300

    Ajustes k

--- src/config.h
+++ src/config.h
@@ -30,7 +30,7 @@
  */
 bool pidActive, targetReached, toggleDisplay;
 double Setpoint, Input, Output;
-double Kp = 3.5, Ki = 0.1, Kd = 0.01;
+double Kp = 3.0, Ki = 0.1, Kd = 0.01;
 uint32_t control_timer, report_timer, off_timer;
 uint32_t duration = DEFAULT_TIME_LIMIT * 60 * 1000;
 
